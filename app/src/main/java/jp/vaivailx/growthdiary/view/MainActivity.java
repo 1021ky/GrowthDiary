802a103c2cba307dcaa5620dc222d6ee0f643d55
k_yamanaka <vaivailx+git@gmail.com>
2017-05-04 11:49:20 +0900

    無駄にFragmentの監視をし続けていたのを修正。ただ、この監視をしておかないと異常終了してしまったはずなので要調査。

--- app/src/main/java/jp/vaivailx/growthdiary/view/MainActivity.java
+++ app/src/main/java/jp/vaivailx/growthdiary/view/MainActivity.java
@@ -65,14 +65,6 @@ public class MainActivity
 
   @Override
   protected void onDestroy() {
-    // Fragmentが終了するまで待つ
-    while(getFragmentManager().isDestroyed()){
-      try {
-        Thread.sleep(500);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
     super.onDestroy();
   }
 
