5921061ab60a11101f0bb898ee100264db2b72e0
k_yamanaka <vaivailx+git@gmail.com>
2017-05-21 17:13:03 +0900

    グラフの表記を修正
    グラフの線のタイトルを修正。

--- app/src/main/java/jp/vaivailx/growthdiary/view/graph/GrowthGraphChartFragment.java
+++ app/src/main/java/jp/vaivailx/growthdiary/view/graph/GrowthGraphChartFragment.java
@@ -28,7 +28,6 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import jp.vaivailx.growthdiary.R;
 import jp.vaivailx.growthdiary.controller.DiaryManager;
-import jp.vaivailx.growthdiary.model.DiaryData;
 import jp.vaivailx.growthdiary.view.DiaryDTO;
 
 /**
@@ -41,267 +40,179 @@ import jp.vaivailx.growthdiary.view.DiaryDTO;
  *
  */
 public class GrowthGraphChartFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+  // TODO: Rename and change types of parameters
+  private String mParam1;
+  private String mParam2;
 
-    private OnGraphFragmentInteractionListener mListener;
+  private OnGraphFragmentInteractionListener mListener;
 
-    @Bind(R.id.chart1)
-    public LineChart mChart;
+  @Bind(R.id.chart1)
+  public LineChart mChart;
 
-    private DiaryManager diaryManager;
+  private DiaryManager diaryManager;
 
-    private AdView  mAdView;
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GrowthGraphChartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GrowthGraphChartFragment newInstance(String param1, String param2) {
-        GrowthGraphChartFragment fragment = new GrowthGraphChartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+  private AdView  mAdView;
+  /**
+   * Use this factory method to create a new instance of
+   * this fragment using the provided parameters.
+   *
+   * @param param1 Parameter 1.
+   * @param param2 Parameter 2.
+   * @return A new instance of fragment GrowthGraphChartFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static GrowthGraphChartFragment newInstance(String param1, String param2) {
+    GrowthGraphChartFragment fragment = new GrowthGraphChartFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    args.putString(ARG_PARAM2, param2);
+    fragment.setArguments(args);
+    return fragment;
+  }
+  public GrowthGraphChartFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mParam1 = getArguments().getString(ARG_PARAM1);
+      mParam2 = getArguments().getString(ARG_PARAM2);
     }
-    public GrowthGraphChartFragment() {
-        // Required empty public constructor
-    }
+    super.onCreate(savedInstanceState);
+    diaryManager = new DiaryManager();
+    diaryManager.onCreate();
+  }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        super.onCreate(savedInstanceState);
-        diaryManager = new DiaryManager();
-        diaryManager.onCreate();
-    }
+  private void setData(int count, float range) {
 
-    private void setData(int count, float range) {
+    List<DiaryDTO> diaryDataList = diaryManager.getLaterDiaryList(count);
+    ArrayList<Entry> values = new ArrayList<Entry>();
 
-      List<DiaryDTO> diaryDataList = diaryManager.getLaterDiaryList(count);
-      ArrayList<Entry> values = new ArrayList<Entry>();
-
-      for (int i = 0; i < diaryDataList.size(); i++ ) {
-        values.add(new Entry(i+1, diaryDataList.get(diaryDataList.size() - 1 - i).evaluation));
-      }
-
-      LineDataSet set1;
-
-//      if (mChart.getData() != null &&
-//              mChart.getData().getDataSetCount() > 0) {
-//          set1 = (LineDataSet)mChart.getData().getDataSetByIndex(0);
-//          set1.setValues(values);
-//          mChart.getData().notifyDataChanged();
-//          mChart.notifyDataSetChanged();
-//      } else {
-        // create a dataset and give it a type
-        set1 = new LineDataSet(values, "evalueation data");
-
-        // set the line to be drawn like this "- - - - - -"
-        set1.enableDashedLine(10f, 5f, 0f);
-        set1.enableDashedHighlightLine(10f, 5f, 0f);
-        set1.setColor(Color.BLACK);
-        set1.setCircleColor(Color.BLACK);
-        set1.setLineWidth(2f);
-        set1.setCircleRadius(3f);
-        set1.setDrawCircleHole(false);
-        set1.setValueTextSize(9f);
-        set1.setDrawFilled(true);
-        set1.setFormLineWidth(1f);
-        set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-        set1.setFormSize(15.f);
-
-//            if (Utils.getSDKInt() >= 18) {
-//                // fill drawable only supported on api level 18 and above
-//                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
-//                set1.setFillDrawable(drawable);
-//            }
-//            else {
-//          set1.setFillColor(Color.BLACK);
-//            }
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(set1); // add the datasets
-
-        // create a data object with the datasets
-        LineData data = new LineData(dataSets);
-
-        // set data
-        mChart.setData(data);
-//        }
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_growth_graph_chart, container, false);
-        ButterKnife.bind(this, view);
-
-        mAdView = (AdView) view.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        setContentView(R.layout.activity_linechart);
-
-
-//        mSeekBarX.setProgress(45);
-//        mSeekBarY.setProgress(100);
-
-//        mSeekBarY.setOnSeekBarChangeListener(this);
-//        mSeekBarX.setOnSeekBarChangeListener(this);
-//
-//        mChart.setOnChartGestureListener(this);
-//        mChart.setOnChartValueSelectedListener(this);
-//        mChart.setDrawGridBackground(false);
-
-        // no description text
-        mChart.getDescription().setEnabled(false);
-
-        // enable touch gestures
-        mChart.setTouchEnabled(true);
-
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        // mChart.setScaleXEnabled(true);
-        // mChart.setScaleYEnabled(true);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-//        mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        // mChart.setBackgroundColor(Color.GRAY);
-
-        // create a custom MarkerView (extend MarkerView) and specify the layout
-        // to use for it
-//        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
-//        mv.setChartView(mChart); // For bounds control
-//        mChart.setMarker(mv); // Set the marker to the chart
-
-        // x-axis limit line
-//        LimitLine llXAxis = new LimitLine(10f, "Index 10");
-//        llXAxis.setLineWidth(4f);
-//        llXAxis.enableDashedLine(10f, 10f, 0f);
-//        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-//        llXAxis.setTextSize(10f);
-
-        XAxis xAxis = mChart.getXAxis();
-      xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.enableGridDashedLine(10f, 10f, 0f);
-        //xAxis.setValueFormatter(new MyCustomXAxisValueFormatter());
-        //xAxis.addLimitLine(llXAxis); // add x-axis limit line
-
-
-//        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "OpenSans-Regular.ttf");
-
-//        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
-//        ll1.setLineWidth(4f);
-//        ll1.enableDashedLine(10f, 10f, 0f);
-//        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
-//        ll1.setTextSize(10f);
-//        ll1.setTypeface(tf);
-
-//        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
-//        ll2.setLineWidth(4f);
-//        ll2.enableDashedLine(10f, 10f, 0f);
-//        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-//        ll2.setTextSize(10f);
-//        ll2.setTypeface(tf);
-
-        YAxis leftAxis = mChart.getAxisLeft();
-        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
-//        leftAxis.addLimitLine(ll1);
-//        leftAxis.addLimitLine(ll2);
-        leftAxis.setAxisMaximum(6f);
-        leftAxis.setAxisMinimum(0f);
-        //leftAxis.setYOffset(20f);
-//        leftAxis.enableGridDashedLine(10f, 10f, 0f);
-        leftAxis.setDrawZeroLine(false);
-
-        // limit lines are drawn behind data (and not on top)
-        leftAxis.setDrawLimitLinesBehindData(true);
-
-        mChart.getAxisRight().setEnabled(false);
-
-        //mChart.getViewPortHandler().setMaximumScaleY(2f);
-        //mChart.getViewPortHandler().setMaximumScaleX(2f);
-
-        // add data
-        setData(30, 5);
-
-//        mChart.setVisibleXRange(20);
-//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
-//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
-
-        mChart.animateX(2500);
-        //mChart.invalidate();
-
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-
-        // modify the legend ...
-//        l.setForm(Legend.LegendForm.LINE);
-
-        // // dont forget to refresh the drawing
-// mChart.invalidate();
-
-        return view;
+    for (int i = 0; i < diaryDataList.size(); i++ ) {
+      values.add(new Entry(i+1, diaryDataList.get(diaryDataList.size() - 1 - i).evaluation));
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    LineDataSet set1;
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnGraphFragmentInteractionListener) {
-            mListener = (OnGraphFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnGraphFragmentInteractionListener");
-        }
+    // create a dataset and give it a type
+    set1 = new LineDataSet(values, getResources().getString(R.string.GRAPH_TITLE));
+
+    // set the line to be drawn like this "- - - - - -"
+    set1.enableDashedLine(10f, 5f, 0f);
+    set1.enableDashedHighlightLine(10f, 5f, 0f);
+    set1.setColor(Color.BLACK);
+    set1.setCircleColor(Color.BLACK);
+    set1.setLineWidth(2f);
+    set1.setCircleRadius(3f);
+    set1.setDrawCircleHole(false);
+    set1.setValueTextSize(9f);
+    set1.setDrawFilled(true);
+    set1.setFormLineWidth(1f);
+    set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+    set1.setFormSize(15.f);
+
+    ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+    dataSets.add(set1); // add the datasets
+
+    // create a data object with the datasets
+    LineData data = new LineData(dataSets);
+
+    // set data
+    mChart.setData(data);
+  }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_growth_graph_chart, container, false);
+    ButterKnife.bind(this, view);
+
+    mAdView = (AdView) view.findViewById(R.id.adView);
+    AdRequest adRequest = new AdRequest.Builder().build();
+    mAdView.loadAd(adRequest);
+
+    // no description text
+    mChart.getDescription().setEnabled(false);
+
+    // enable touch gestures
+    mChart.setTouchEnabled(true);
+
+    // enable scaling and dragging
+    mChart.setDragEnabled(true);
+    mChart.setScaleEnabled(true);
+
+    XAxis xAxis = mChart.getXAxis();
+    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+    xAxis.enableGridDashedLine(10f, 10f, 0f);
+
+    YAxis leftAxis = mChart.getAxisLeft();
+    leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+
+    leftAxis.setAxisMaximum(6f);
+    leftAxis.setAxisMinimum(0f);
+
+    leftAxis.setDrawZeroLine(false);
+
+    // limit lines are drawn behind data (and not on top)
+    leftAxis.setDrawLimitLinesBehindData(true);
+
+    mChart.getAxisRight().setEnabled(false);
+
+    // add data
+    setData(30, 5);
+
+    mChart.animateX(2500);
+
+    // get the legend (only possible after setting data)
+    Legend l = mChart.getLegend();
+
+    return view;
+  }
+
+  // TODO: Rename method, update argument and hook method into UI event
+  public void onButtonPressed(Uri uri) {
+    if (mListener != null) {
+      mListener.onFragmentInteraction(uri);
     }
+  }
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof OnGraphFragmentInteractionListener) {
+      mListener = (OnGraphFragmentInteractionListener) context;
+    } else {
+      throw new RuntimeException(context.toString()
+              + " must implement OnGraphFragmentInteractionListener");
+    }
+  }
 
   @Override
   public void onDetach() {
-
+    super.onDetach();
   }
 
   public void update() {
   }
 
   /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnGraphFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+   * This interface must be implemented by activities that contain this
+   * fragment to allow an interaction in this fragment to be communicated
+   * to the activity and potentially other fragments contained in that
+   * activity.
+   * <p>
+   * See the Android Training lesson <a href=
+   * "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface OnGraphFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+  }
 }
